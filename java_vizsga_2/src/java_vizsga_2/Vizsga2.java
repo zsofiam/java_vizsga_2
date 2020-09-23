5e927d983920f22ddf9027bcb1b030d03e615424
zsofiam <zsofia.matyasi@gmail.com>
2020-09-23 09:10:18 +0200

    add task

--- java_vizsga_2/src/java_vizsga_2/Vizsga2.java
+++ java_vizsga_2/src/java_vizsga_2/Vizsga2.java
@@ -2,4 +2,12 @@ package java_vizsga_2;
 
 public class Vizsga2 {
 
+	public static void main(String[]args) {
+		System.out.println("2.	Parancssorban kiírjuk az összes primitív változó"
+				+ " minimum és maximum értékét, ha van ilyen.\r\n" + 
+				"3.	Boolean változó ismeretében byte változót állítgatunk"
+				+ " (true => 1, false => 0) adatbekérés alapján. \r\n" + 
+				"");
+		
+	}
 }
