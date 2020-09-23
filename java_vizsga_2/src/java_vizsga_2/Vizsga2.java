051ade217906329abc1fa5b2d6ea580aaccf701f
zsofiam <zsofia.matyasi@gmail.com>
2020-09-23 09:51:50 +0200

    solve task 2

--- java_vizsga_2/src/java_vizsga_2/Vizsga2.java
+++ java_vizsga_2/src/java_vizsga_2/Vizsga2.java
@@ -2,12 +2,29 @@ package java_vizsga_2;
 
 public class Vizsga2 {
 
-	public static void main(String[]args) {
-		System.out.println("2.	Parancssorban kiírjuk az összes primitív változó"
-				+ " minimum és maximum értékét, ha van ilyen.\r\n" + 
-				"3.	Boolean változó ismeretében byte változót állítgatunk"
-				+ " (true => 1, false => 0) adatbekérés alapján. \r\n" + 
-				"");
+	public static void main(String[] args) {
+		System.out.println("2. feladat");
+		System.out.println("Parancssorban kiírjuk az összes primitív"
+				+ " változó minimum és maximum értékét, ha van ilyen.");
+		System.out.println();
+		System.out.println("Primitív változók minimum és maximum értékei");
+		displayMinAndMaxValue(Byte.TYPE, Byte.MIN_VALUE, Byte.MAX_VALUE);
+		displayMinAndMaxValue(Short.TYPE, Short.MIN_VALUE, Short.MAX_VALUE);
+		displayMinAndMaxValue(Character.TYPE, (int) Character.MIN_VALUE, (int) Character.MAX_VALUE);
+		displayMinAndMaxValue(Integer.TYPE, Integer.MIN_VALUE, Integer.MAX_VALUE);
+		displayMinAndMaxValue(Long.TYPE, Long.MIN_VALUE, Long.MAX_VALUE);
+		displayMinAndMaxValue(Float.TYPE, Float.MIN_VALUE, Float.MAX_VALUE);
+		displayMinAndMaxValue(Double.TYPE, Double.MIN_VALUE, Double.MAX_VALUE);
+		displayMinAndMaxValue(Boolean.TYPE, 0, 1);
 		
+		System.out.println();
+		System.out.println("3. feladat");
+		System.out.println("Boolean változó ismeretében"
+				+ " byte változót állítgatunk (true => 1, false => 0) adatbekérés alapján");
+		System.out.println();
+	}
+
+	public static void displayMinAndMaxValue(Class<?> type, Number min, Number max) {
+		System.out.printf("típus: %-8s min: %-22s max: %s\n", type, min, max);
 	}
 }
